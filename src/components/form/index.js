5cf9f3cdc1ba7d69a38eb3bbdd12b8e62e0b53aa
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-11-26 10:53:18 +0100

    added event to textarea

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -4,6 +4,8 @@ class Form extends Component {
     constructor(props) {
         super(props);
         this.handleTitle = this.handleTitle.bind(this);
+        this.handleTextArea = this.handleTextArea.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
         this.state = {
             title: 'Lorem ipsum',
             description: 'Opis',
@@ -14,6 +16,19 @@ class Form extends Component {
     handleTitle(event) {
         this.setState({
             title: event.target.value
+        })
+    }
+
+    handleTextArea(event) {
+        this.setState({
+            description: event.target.value
+        })
+
+    }
+
+    handleSelect(event) {
+        this.setState({
+            category: event.target.value
         })
     }
 
@@ -28,6 +43,12 @@ class Form extends Component {
                     <label>
                         Name:
                     </label>
+                    <textarea onChange={this.handleTextArea}/>
+                    <select onChange={this.handleSelect} >
+                        <option> Opcja 1 </option>
+                        <option> Opcja 2 </option>
+                        <option> Opcja 3 </option>
+                    </select>
                     <input type="text" onChange={this.handleTitle}/>
                     <input type="submit" value="Submit"/>
                 </div>
