8fbcef857c59055bd55d9915a29d8b38e48e5978
Wojciech <wojciech.paluch@o2.pl>
2017-12-02 13:21:37 +0100

    Added array

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -2,10 +2,25 @@ import React, {Component} from 'react';
 
 class Users extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = [{
+            name: 'Ala'
+        }, {
+            name: 'Ola'
+        }, {
+            name: 'Jola'
+        }];
+    }
+
+
     render() {
         return (
             <div>
                 <h1> Users </h1>
+                {this.state.map((users, index) => (
+                    <div key={index}>{users.name}</div>
+                ))}
             </div>
         );
     }
