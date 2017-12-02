2d6185c72f3410c85a9d877e1867956f9c16b190
Wojciech <wojciech.paluch@o2.pl>
2017-12-02 13:52:03 +0100

    added loading

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -6,6 +6,7 @@ class Users extends Component {
         super(props);
         this.fetchData = this.fetchData.bind(this);
         this.state = {
+            loading: false,
             users: [{
                 name: 'Ala'
             }, {
@@ -18,19 +19,27 @@ class Users extends Component {
 
 
     fetchData() {
+        this.setState({loading: true});
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                this.setState({users: data});
+                this.setState({loading: false, users: data});
             })
+    }
+
+    renderIndicator(){
+        if (this.state.loading) {
+            return <div style={{color: 'red', fontSize: 15}}> Loading...</div>;
+        }
     }
 
     render() {
         return (
             <div>
                 <h1> Users </h1>
+                {this.renderIndicator()}
                 <button onClick={this.fetchData}>Pobierz dane</button>
                 {this.state.users.map((user, index) => (
                     <div key={index}>{user.name}</div>
